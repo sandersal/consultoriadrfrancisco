967a9b49f2f12b0fba6befba7120aceaadec7077
sandersal <49814328+sandersal@users.noreply.github.com>
2025-10-20 13:37:55 +0000

    build ajustado para github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: './', // ✅ Corrigido para funcionar no GitHub Pages
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
